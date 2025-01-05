69aced5ba5c10c8ece9d2c438820c211bd11608a
Dale Ribeiro <dale@fleetdm.com>
2025-01-05 12:34:49 -0500

    Initial Commit

---
+++ StickTechnology/StickTechnologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickTechnologyApp.swift
+//  StickTechnology
+//
+//  Created by Dale Ribeiro on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StickTechnologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
